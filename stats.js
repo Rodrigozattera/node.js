3e05f96e6bb194b49ac0e1ee8636cbcac2edda2d
Diguin <Thunderzats@gmail.com>
2020-09-19 20:28:17 -0300

    commit

--- stats.js
+++ stats.js
@@ -1,17 +1,14 @@
 const os = require('os');
+const log = require('./logger')
 
 setInterval(() => {
-  //console.log(os.platform())
-  
+
   const { freemem,  totalmem } = os;
-  
-  //console.log(`${parseInt(freemem() / 1024 / 1024)} MB` , totalmem())
+
   const total = parseInt(totalmem() / 1024 / 1024)
   const mem = parseInt(freemem() / 1024 / 1024)
   const percents = parseInt((mem / total) * 100)
-  
-  //console.log(mem, total, percents)
-  
+
   const stats = {
       free:  `${mem} MB`,
       total: `${total} MB`,
@@ -20,5 +17,7 @@ setInterval(() => {
   console.clear()
   console.log("=== PC STATS ===")
   console.table(stats)
+
+  log(`${JSON.stringify(stats)} \n`)
 
 }, 1000)
